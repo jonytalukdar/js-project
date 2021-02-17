cd21ac678e5217ddfab0b02c1ac235964d6678e3
jonytalukdar <jonytalukdar30@gmail.com>
2021-02-17 23:21:38 +0600

    completed created new list

--- Todo-app/app.js
+++ Todo-app/app.js
@@ -1,17 +1,33 @@
 const listsContainer = document.querySelector('[data-lists]');
+const newListForm = document.querySelector('[data-new-list-form]');
+const newListInput = document.querySelector('[data-new-list-input]');
 
-let lists = ['name', 'todo'];
+let lists = [
+  {
+    id: 1,
+    name: 'name',
+  },
+  {
+    id: 1,
+    name: 'todo',
+  },
+];
 
 function render() {
   clearElement(listsContainer);
   lists.forEach((list) => {
     const listElement = document.createElement('li');
+    listElement.dataset.listId = list.id;
     listElement.classList.add('list-name');
-    listElement.innerText = list;
+    listElement.innerText = list.name;
     listsContainer.appendChild(listElement);
   });
 }
 
-function clearElement(element) {}
+function clearElement(element) {
+  while (element.firstChild) {
+    element.removeChild(element.firstChild);
+  }
+}
 
 render();
