2169c0c34b9ee932422f39693cd0b8b4c565a498
jonytalukdar <jonytalukdar30@gmail.com>
2021-02-18 16:09:35 +0600

    completed book list

--- book-list/app.js
+++ book-list/app.js
@@ -12,6 +12,21 @@ const year = document.getElementById('year');
 const addButton = document.getElementById('add-button');
 const bookList = document.getElementById('book-list');
 
-addButton.addEventListener('click', function () {
-  console.log('hi');
+addButton.addEventListener('click', function (e) {
+  e.preventDefault();
+  if (title.value === '' || author.value === '' || year.value === '') {
+    alert('Please enter All kind of Information');
+  } else {
+    const tbody = document.getElementById('book-list');
+    const tableRow = document.createElement('tr');
+    tableRow.innerHTML = `
+    <th>${title.value}</th>
+    <th>${author.value}</th>
+    <th>${year.value}</th>
+    `;
+    tbody.appendChild(tableRow);
+  }
+  title.value = '';
+  author.value = '';
+  year.value = '';
 });
