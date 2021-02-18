817a4fa7318631d18a334322ef54df29d1de3c83
jonytalukdar <jonytalukdar30@gmail.com>
2021-02-18 21:15:14 +0600

    select all element

---
+++ calculator/app.js
@@ -0,0 +1,7 @@
+const numberButtons = document.querySelectorAll('[data-number]');
+const operationButtons = document.querySelectorAll('[data-operation]');
+const equalButton = document.querySelector('[data-equal]');
+const deleteButton = document.querySelector('[data-delete]');
+const allClearButton = document.querySelector('[data-all-clear]');
+const previousResult = document.querySelector('[data-previous]');
+const currentResult = document.querySelector('[data-current]');
