d2d05375706372ea4698cd6a27e439a8e23164ad
jonytalukdar <jonytalukdar30@gmail.com>
2021-02-18 15:46:25 +0600

    selected all element

---
+++ book-list/app.js
@@ -0,0 +1,17 @@
+/*
+DOM selection
+add event listener
+basic validation
+create element
+append 
+*/
+
+const title = document.getElementById('title');
+const author = document.getElementById('author');
+const year = document.getElementById('year');
+const addButton = document.getElementById('add-button');
+const bookList = document.getElementById('book-list');
+
+addButton.addEventListener('click', function () {
+  console.log('hi');
+});
