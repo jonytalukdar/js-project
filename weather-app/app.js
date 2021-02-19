c645f9fdabe77734d32c61dd6b1c707de2a06396
jonytalukdar <jonytalukdar30@gmail.com>
2021-02-19 23:36:54 +0600

    completed axios part

--- weather-app/app.js
+++ weather-app/app.js
@@ -12,5 +12,33 @@ const historyElm = document.getElementById('history');
 const masterHistory = document.getElementById('master-history');
 
 const API_KEY = '08d1ff27ba3f8236c3b68e4278207732';
-const BASE_URL = `api.openweathermap.org/data/2.5/weather?q={city name}&appid=${API_KEY}`;
+const BASE_URL = `https://api.openweathermap.org/data/2.5/weather?appid=${API_KEY}`;
 const ICON_API = `http://openweathermap.org/img/wn/`;
+const DEFAULT_CITY = 'tangail,bd';
+
+window.onload = function () {
+  navigator.geolocation.getCurrentPosition(
+    (s) => {
+      getWeatherData(null, s.coords);
+    },
+    (e) => {
+      getWeatherData(DEFAULT_CITY);
+    }
+  );
+};
+
+function getWeatherData(city = DEFAULT_CITY, coords) {
+  let url = BASE_URL;
+
+  city === null
+    ? (url = `${url}&lat=${coords.latitude}&lon=${coords.longitude}`)
+    : (url = `${url}&q=${city}`);
+  axios
+    .get(url)
+    .then((response) => {
+      console.log(response.data);
+    })
+    .catch((e) => {
+      console.log(e);
+    });
+}
