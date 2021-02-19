64624bec994fe68d7bfad345c3417202c881f43f
jonytalukdar <jonytalukdar30@gmail.com>
2021-02-19 12:17:28 +0600

    added all api key and url

--- weather-app/app.js
+++ weather-app/app.js
@@ -10,3 +10,7 @@ const humidity = document.getElementById('humidity');
 const cityInput = document.getElementById('city-input');
 const historyElm = document.getElementById('history');
 const masterHistory = document.getElementById('master-history');
+
+const API_KEY = '08d1ff27ba3f8236c3b68e4278207732';
+const BASE_URL = `api.openweathermap.org/data/2.5/weather?q={city name}&appid=${API_KEY}`;
+const ICON_API = `http://openweathermap.org/img/wn/`;
