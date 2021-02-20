289e523d1239584b818acdd58cedd45185bc645b
jonytalukdar <jonytalukdar30@gmail.com>
2021-02-20 20:57:46 +0600

    completd backend site

--- weather-app/index.js
+++ weather-app/index.js
@@ -9,7 +9,18 @@ app.use(cors());
 
 app.use(express.static('public'));
 
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use('/api/history', require('./api.route'));
+
 const PORT = process.env.PORT || 1212;
 app.listen(PORT, () => {
   console.log('app is runing on PORT 1212');
+  mongoose.connect(
+    `mongodb+srv://joney:joney1@cluster0.2furb.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`,
+    { useNewUrlParser: true },
+    () => {
+      console.log('database connected');
+    }
+  );
 });
