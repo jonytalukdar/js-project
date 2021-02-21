5c7f586fab91b6de606126b223c9b44efe15c0d1
jonytalukdar <jonytalukdar30@gmail.com>
2021-02-21 14:37:02 +0600

    completed stop watch

--- stop-watch/app.js
+++ stop-watch/app.js
@@ -7,19 +7,42 @@ const pause = document.getElementById('pause');
 
 let min;
 let sec = 0;
+let timer = null;
+let watchRunning = false;
 
 function startTime() {
-  setInterval(function () {
-    sec++;
-  }, 1000);
+  if (!watchRunning) {
+    watchRunning = true;
+    timer = setInterval(function () {
+      sec++;
+      let { min, second } = getTime(sec);
+      minutes.innerHTML = min < 10 ? '0' + min : min;
+      seconds.innerHTML = second < 10 ? '0' + second : second;
+    }, 1000);
+  }
 }
 
 function resetTime() {
-  console.log('reset');
+  watchRunning = false;
+  clearInterval(timer);
+  min = 0;
+  sec = 0;
+  minutes.innerHTML = '00';
+  seconds.innerHTML = '00';
 }
 
 function pasueTime() {
-  console.log('pause');
+  watchRunning = false;
+  clearInterval(timer);
+}
+
+function getTime(sec) {
+  min = parseInt(sec / 60); // 122 / 60 = 2.somethign
+  second = parseInt(sec % 60);
+  return {
+    min,
+    second,
+  };
 }
 
 start.addEventListener('click', startTime);
