1ac16f07426e60b4b456762e1786528e45413b27
jonytalukdar <jonytalukdar30@gmail.com>
2021-02-21 13:17:02 +0600

    set function for all time

--- stop-watch/app.js
+++ stop-watch/app.js
@@ -1,5 +1,27 @@
 const minutes = document.getElementById('minutes');
 const seconds = document.getElementById('seconds');
+const miliseconds = document.getElementById('miliseconds');
 const start = document.getElementById('start');
 const reset = document.getElementById('reset');
 const pause = document.getElementById('pause');
+
+let min;
+let sec = 0;
+
+function startTime() {
+  setInterval(function () {
+    sec++;
+  }, 1000);
+}
+
+function resetTime() {
+  console.log('reset');
+}
+
+function pasueTime() {
+  console.log('pause');
+}
+
+start.addEventListener('click', startTime);
+reset.addEventListener('click', resetTime);
+pause.addEventListener('click', pasueTime);
