c5b92143e71b600779907464d54a86a925ab0bc7
jonytalukdar <jonytalukdar30@gmail.com>
2021-02-21 12:20:35 +0600

    completed time function

--- digital-watch/app.js
+++ digital-watch/app.js
@@ -2,3 +2,17 @@ const hour = document.getElementById('hour');
 const minute = document.getElementById('minute');
 const second = document.getElementById('second');
 const progressBar = document.getElementById('progress');
+
+const showTime = () => {
+  let date = new Date();
+  let hr = date.getHours();
+  let min = date.getMinutes();
+  let sec = date.getSeconds();
+  hour.innerHTML = hr;
+  minute.innerHTML = min;
+  second.innerHTML = sec;
+};
+
+setInterval(() => {
+  showTime();
+});
