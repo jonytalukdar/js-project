158934e1b8852ceaf241f2b5d98f569d45e83eb4
jonytalukdar <jonytalukdar30@gmail.com>
2021-02-21 12:56:17 +0600

    selected all elements

---
+++ stop-watch/app.js
@@ -0,0 +1,5 @@
+const minutes = document.getElementById('minutes');
+const seconds = document.getElementById('seconds');
+const start = document.getElementById('start');
+const reset = document.getElementById('reset');
+const pause = document.getElementById('pause');
