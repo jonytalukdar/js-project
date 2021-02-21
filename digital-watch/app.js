d725f2bf9c63a221479d72aa6227e11e5bbf7e11
jonytalukdar <jonytalukdar30@gmail.com>
2021-02-21 12:31:58 +0600

    completed digital watch

--- digital-watch/app.js
+++ digital-watch/app.js
@@ -4,15 +4,17 @@ const second = document.getElementById('second');
 const progressBar = document.getElementById('progress');
 
 const showTime = () => {
-  let date = new Date();
-  let hr = date.getHours();
-  let min = date.getMinutes();
-  let sec = date.getSeconds();
+  let time = new Date();
+  let hr = time.getHours();
+  let min = time.getMinutes();
+  let sec = time.getSeconds();
   hour.innerHTML = hr;
   minute.innerHTML = min;
   second.innerHTML = sec;
+
+  progressBar.style.width = (sec / 60) * 100 + '%';
 };
 
 setInterval(() => {
   showTime();
-});
+}, 1000);
